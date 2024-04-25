f1fc72f5c5e24cc4ce1f0bf3974716def1d3d18d
rocio-fernandez <rocio.fernandezt@opendeusto.es>
2024-04-26 01:46:07 +0200

    JDOs javadoc

--- DeustoCars/src/main/java/es/deusto/spq/server/jdo/User.java
+++ DeustoCars/src/main/java/es/deusto/spq/server/jdo/User.java
@@ -1,59 +1,107 @@
 package es.deusto.spq.server.jdo;
 
 import java.util.Set;
-
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.PrimaryKey;
 import javax.jdo.annotations.Join;
 import javax.jdo.annotations.Persistent;
 import java.util.LinkedHashSet;
 
+/**
+ * Represents a user entity.
+ */
 @PersistenceCapable
 public class User {
-	@PrimaryKey
-	String login=null;
-	String password=null;
-	
-	@Persistent(mappedBy="user", dependentElement="true")
-	@Join
-	Set<Message> messages = new LinkedHashSet<>();
-	
-	User() {
+    @PrimaryKey
+    String login;
+    String password;
 
-	}
-	
-	public User(String login, String password) {
-		this.login = login;
-		this.password = password;
-	}
-	
-	public void addMessage(Message message) {
-		messages.add(message);
-	}
+    @Persistent(mappedBy="user", dependentElement="true")
+    @Join
+    Set<Message> messages = new LinkedHashSet<>();
 
-	public void removeMessage(Message message) {
-		messages.remove(message);
-	}
+    /**
+     * Default constructor for User.
+     */
+    User() {
+    }
 
-	public String getLogin() {
-		return this.login;
-	}
-	
-	public String getPassword() {
-		return this.password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	 public Set<Message> getMessages() {return this.messages;}
-	 
-	 public String toString() {
-		StringBuilder messagesStr = new StringBuilder();
-		for (Message message: this.messages) {
-			messagesStr.append(message.toString() + " - ");
-		}
+    /**
+     * Constructs a User object with the specified login and password.
+     * 
+     * @param login    The login of the user.
+     * @param password The password of the user.
+     */
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    /**
+     * Adds a message to the user's set of messages.
+     * 
+     * @param message The message to add.
+     */
+    public void addMessage(Message message) {
+        messages.add(message);
+    }
+
+    /**
+     * Removes a message from the user's set of messages.
+     * 
+     * @param message The message to remove.
+     */
+    public void removeMessage(Message message) {
+        messages.remove(message);
+    }
+
+    /**
+     * Retrieves the login of the user.
+     * 
+     * @return The login of the user.
+     */
+    public String getLogin() {
+        return this.login;
+    }
+
+    /**
+     * Retrieves the password of the user.
+     * 
+     * @return The password of the user.
+     */
+    public String getPassword() {
+        return this.password;
+    }
+
+    /**
+     * Sets the password of the user.
+     * 
+     * @param password The password to set.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Retrieves the set of messages associated with the user.
+     * 
+     * @return The set of messages associated with the user.
+     */
+    public Set<Message> getMessages() {
+        return this.messages;
+    }
+
+    /**
+     * Returns a string representation of the User object.
+     * 
+     * @return A string representation of the User object.
+     */
+    @Override
+    public String toString() {
+        StringBuilder messagesStr = new StringBuilder();
+        for (Message message: this.messages) {
+            messagesStr.append(message.toString() + " - ");
+        }
         return "User: login --> " + this.login + ", password --> " + this.password + ", messages --> [" + messagesStr + "]";
     }
 }
