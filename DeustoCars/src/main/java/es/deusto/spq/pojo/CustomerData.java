ed08bc20a72382bd5b7885e6657e0372f484cb2e
rocio-fernandez <rocio.fernandezt@opendeusto.es>
2024-04-26 01:38:26 +0200

    POJOs javadoc

--- DeustoCars/src/main/java/es/deusto/spq/pojo/CustomerData.java
+++ DeustoCars/src/main/java/es/deusto/spq/pojo/CustomerData.java
@@ -2,81 +2,134 @@ package es.deusto.spq.pojo;
 
 import java.util.Date;
 
-/*
- * Class of the customer
- * */
-
+/**
+ * Represents customer data.
+ */
 public class CustomerData {
     private String eMail;
     private String name;
     private String surname;
     private Date dateOfBirth;
 
-    /*
-     * Constructor of the customer class
-     * */
-
-
+    /**
+     * Constructs a customer data object with the specified email, name, surname, and date of birth.
+     *
+     * @param eMail       The email of the customer.
+     * @param name        The name of the customer.
+     * @param surname     The surname of the customer.
+     * @param dateOfBirth The date of birth of the customer.
+     */
     public CustomerData(String eMail, String name, String surname, Date dateOfBirth) {
         this.eMail = eMail;
         this.name = name;
         this.surname = surname;
         this.dateOfBirth = dateOfBirth;
     }
+
+    /**
+     * Constructs a customer data object with the specified email, name, and surname.
+     *
+     * @param eMail   The email of the customer.
+     * @param name    The name of the customer.
+     * @param surname The surname of the customer.
+     */
     public CustomerData(String eMail, String name, String surname) {
         this.eMail = eMail;
         this.name = name;
         this.surname = surname;
     }
 
-    public CustomerData(){
+    /**
+     * Default constructor required for serialization.
+     */
+    public CustomerData() {
         // Required for serialization
     }
-    
-    /*
-     * Getters and setters for the customer class
-     * */
+
+    /**
+     * Retrieves the email of the customer.
+     *
+     * @return The email of the customer.
+     */
     public String geteMail() {
         return eMail;
     }
 
+    /**
+     * Sets the email of the customer.
+     *
+     * @param eMail The email to set.
+     */
     public void seteMail(String eMail) {
         this.eMail = eMail;
     }
 
+    /**
+     * Retrieves the name of the customer.
+     *
+     * @return The name of the customer.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the customer.
+     *
+     * @param name The name to set.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Retrieves the surname of the customer.
+     *
+     * @return The surname of the customer.
+     */
     public String getSurname() {
         return surname;
     }
 
-	public void setSurname(String surname) {
+    /**
+     * Sets the surname of the customer.
+     *
+     * @param surname The surname to set.
+     */
+    public void setSurname(String surname) {
         this.surname = surname;
     }
 
+    /**
+     * Retrieves the date of birth of the customer.
+     *
+     * @return The date of birth of the customer.
+     */
     public Date getDateOfBirth() {
         return dateOfBirth;
     }
 
+    /**
+     * Sets the date of birth of the customer.
+     *
+     * @param dateOfBirth The date of birth to set.
+     */
     public void setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
-	@Override
-	public String toString() {
-		return "Customer [eMail=" + eMail + ", name=" + name + ", surname=" + surname + "]";//", dateOfBirth=" + dateOfBirth + "]";
-	}
-
-    
-    /*
-     * Returns a string representation of the Customer object.
-     * */
-    
-    
+    /**
+     * Returns a string representation of the CustomerData object.
+     *
+     * @return A string representation of the CustomerData object.
+     */
+    @Override
+    public String toString() {
+        return "CustomerData{" +
+                "eMail='" + eMail + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                //", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
 }
--- DeustoCars/src/main/java/es/deusto/spq/pojo/VehicleData.java
+++ DeustoCars/src/main/java/es/deusto/spq/pojo/VehicleData.java
@@ -1,9 +1,8 @@
 package es.deusto.spq.pojo;
 
 /**
- * Class that represents a vehicle.
+ * Represents a vehicle.
  */
-
 public class VehicleData {
     private String numberPlate;
     private String brand;
@@ -12,11 +11,12 @@ public class VehicleData {
     private boolean onRepair;
     
     /**
-     * Vehicle class constructor.
-     * 
+     * Constructs a VehicleData object with the specified number plate, brand, and model.
+     *
+     * @param numberPlate The number plate of the vehicle.
+     * @param brand       The brand of the vehicle.
+     * @param model       The model of the vehicle.
      */
-    
-    
     public VehicleData(String numberPlate, String brand,  String model) {
         this.numberPlate = numberPlate;
         this.brand = brand;
@@ -25,70 +25,116 @@ public class VehicleData {
         this.onRepair = false;
     }
 
+    /**
+     * Default constructor required for serialization.
+     */
     public VehicleData(){
         // Required for serialization
     }
 
-
     /**
-     * Getter and setter methods of the Vehicle class.
-     * 
+     * Retrieves the number plate of the vehicle.
+     *
+     * @return The number plate of the vehicle.
      */
-
     public String getNumberPlate() {
         return numberPlate;
     }
 
+    /**
+     * Sets the number plate of the vehicle.
+     *
+     * @param numberPlate The number plate to set.
+     */
     public void setNumberPlate(String numberPlate) {
         this.numberPlate = numberPlate;
     }
 
+    /**
+     * Retrieves the brand of the vehicle.
+     *
+     * @return The brand of the vehicle.
+     */
     public String getBrand() {
         return brand;
     }
 
+    /**
+     * Sets the brand of the vehicle.
+     *
+     * @param brand The brand to set.
+     */
     public void setBrand(String brand) {
         this.brand = brand;
     }
 
-
-
+    /**
+     * Retrieves the model of the vehicle.
+     *
+     * @return The model of the vehicle.
+     */
     public String getModel() {
         return model;
     }
 
+    /**
+     * Sets the model of the vehicle.
+     *
+     * @param model The model to set.
+     */
     public void setModel(String model) {
         this.model = model;
     }
 
+    /**
+     * Checks if the vehicle is ready to borrow.
+     *
+     * @return True if the vehicle is ready to borrow, false otherwise.
+     */
     public boolean isReadyToBorrow() {
         return readyToBorrow;
     }
 
+    /**
+     * Sets whether the vehicle is ready to borrow.
+     *
+     * @param readyToBorrow True if the vehicle is ready to borrow, false otherwise.
+     */
     public void setReadyToBorrow(boolean readyToBorrow) {
         this.readyToBorrow = readyToBorrow;
     }
 
+    /**
+     * Checks if the vehicle is on repair.
+     *
+     * @return True if the vehicle is on repair, false otherwise.
+     */
     public boolean isOnRepair() {
         return onRepair;
     }
 
+    /**
+     * Sets whether the vehicle is on repair.
+     *
+     * @param onRepair True if the vehicle is on repair, false otherwise.
+     */
     public void setOnRepair(boolean onRepair) {
         this.onRepair = onRepair;
     }
     
     /**
-     * Returns a string representation of the Vehicle object.
-     * 
+     * Returns a string representation of the VehicleData object.
+     *
+     * @return A string representation of the VehicleData object.
      */
-    
+    @Override
     public String toString() {
-        return "Vehicle" + 
-               "\nNumber Plate: " + numberPlate + 
-               "\nBrand: " + brand + 
-               "\nModel: " + model + 
-               "\nReady to Borrow: " + readyToBorrow + 
-               "\nOn Repair: " + onRepair;
+        return "VehicleData{" +
+               "\nNumber Plate: " + numberPlate +
+               "\nBrand: " + brand +
+               "\nModel: " + model +
+               "\nReady to Borrow: " + readyToBorrow +
+               "\nOn Repair: " + onRepair +
+               '}';
     }
 }
-
