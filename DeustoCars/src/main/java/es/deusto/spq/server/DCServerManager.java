3c5c3117b5ad69979b65ed3ec30444ebac9bbbeb
rocio-fernandez <rocio.fernandezt@opendeusto.es>
2024-04-26 01:48:08 +0200

    Server javadoc

--- DeustoCars/src/main/java/es/deusto/spq/server/DCServerManager.java
+++ DeustoCars/src/main/java/es/deusto/spq/server/DCServerManager.java
@@ -29,148 +29,173 @@ import org.apache.logging.log4j.Logger;
 import es.deusto.spq.serialization.Customer;
 import es.deusto.spq.serialization.Vehicle;
 
-// Server manager will serve as a test class for the server functions, when running the app with the
-// true client it is not necessary to run: mvn exec:java -Pserver
+/**
+ * Represents a manager for interacting with the DeustoCars server.
+ */
 public class DCServerManager implements ActionListener, Runnable {
 
-	protected static final Logger logger = LogManager.getLogger();
+    protected static final Logger logger = LogManager.getLogger();
 
-	private JFrame frame;
-	private JButton buttonEnd;
-	private JLabel customerLabel;
-	private JTextField customerText;
-	private JLabel vehicleLabel;
-	private JTextField vehicleText;
-	private JLabel message;
+    private JFrame frame;
+    private JButton buttonEnd;
+    private JLabel customerLabel;
+    private JTextField customerText;
+    private JLabel vehicleLabel;
+    private JTextField vehicleText;
+    private JLabel message;
 
-	private Client client;
-	private WebTarget webTarget;
+    private Client client;
+    private WebTarget webTarget;
 
-	private Thread thread;
-	private final AtomicBoolean running = new AtomicBoolean(false);
-	
-	public DCServerManager(String hostname, String port) {
-		client = ClientBuilder.newClient();
-		// this is the default server path, then the extensions for each method should be added
-		// the 'deustocars' extension part should match the web.xml file
-		// probably if pom not changed http://localhost:8080/deustocars/...
-		webTarget = client.target(String.format("http://%s:%s/deustocars", hostname, port));
+    private Thread thread;
+    private final AtomicBoolean running = new AtomicBoolean(false);
 
-		this.buttonEnd = new JButton("End DCServer Process");
-		this.buttonEnd.addActionListener(this);
-		this.message = new JLabel("REST DeustoCars Manager running...");
-		this.message.setOpaque(true);
-		this.message.setForeground(Color.yellow);
-		this.message.setBackground(Color.gray);
+    /**
+     * Constructs a new DCServerManager with the given hostname and port.
+     * 
+     * @param hostname The hostname of the server.
+     * @param port     The port of the server.
+     */
+    public DCServerManager(String hostname, String port) {
+        client = ClientBuilder.newClient();
+        webTarget = client.target(String.format("http://%s:%s/deustocars", hostname, port));
 
-		JPanel panelSuperior = new JPanel();
-		this.customerLabel = new JLabel("Customers:");
-		this.vehicleLabel = new JLabel("Vehicles:");
-		panelSuperior.add(this.customerLabel);
-		panelSuperior.add(this.vehicleLabel);
-		this.customerText = new JTextField("...");
-		this.vehicleText = new JTextField("...");
-		this.customerText.setPreferredSize( new Dimension( 200, 24 ) );
-		this.vehicleText.setPreferredSize( new Dimension( 200, 24 ) );
-		panelSuperior.add(this.customerText);
-		panelSuperior.add(this.vehicleText);
+        this.buttonEnd = new JButton("End DCServer Process");
+        this.buttonEnd.addActionListener(this);
+        this.message = new JLabel("REST DeustoCars Manager running...");
+        this.message.setOpaque(true);
+        this.message.setForeground(Color.yellow);
+        this.message.setBackground(Color.gray);
 
-		JPanel panelBoton = new JPanel();
-		panelBoton.add(this.buttonEnd);
+        JPanel panelSuperior = new JPanel();
+        this.customerLabel = new JLabel("Customers:");
+        this.vehicleLabel = new JLabel("Vehicles:");
+        panelSuperior.add(this.customerLabel);
+        panelSuperior.add(this.vehicleLabel);
+        this.customerText = new JTextField("...");
+        this.vehicleText = new JTextField("...");
+        this.customerText.setPreferredSize(new Dimension(200, 24));
+        this.vehicleText.setPreferredSize(new Dimension(200, 24));
+        panelSuperior.add(this.customerText);
+        panelSuperior.add(this.vehicleText);
 
-		this.frame = new JFrame("DeustoCars: Server Manager");
-		this.frame.setSize(1000, 500);
-		this.frame.setResizable(false);		
-		this.frame.getContentPane().add(panelSuperior, "North");
-		this.frame.getContentPane().add(panelBoton);
-		this.frame.getContentPane().add(this.message, "South");
-		this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		//WindowManager.middleCenter(this.frame);
-		this.frame.setVisible(true);
+        JPanel panelBoton = new JPanel();
+        panelBoton.add(this.buttonEnd);
 
-		thread = new Thread(this);
-		thread.start();
-	}
+        this.frame = new JFrame("DeustoCars: Server Manager");
+        this.frame.setSize(1000, 500);
+        this.frame.setResizable(false);
+        this.frame.getContentPane().add(panelSuperior, "North");
+        this.frame.getContentPane().add(panelBoton);
+        this.frame.getContentPane().add(this.message, "South");
+        this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.frame.setVisible(true);
 
-	public static void main(String[] args) {
-		String hostname = args[0];
-		String port = args[1];
+        thread = new Thread(this);
+        thread.start();
+    }
 
-		new DCServerManager(hostname, port);
-	}
+    /**
+     * Main method to start the DCServerManager.
+     * 
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        String hostname = args[0];
+        String port = args[1];
 
-	public String getCustomersString(){
-		WebTarget DeustoCarsWebTarget = webTarget.path("server/getcustomers");
-		Invocation.Builder invocationBuilder = DeustoCarsWebTarget.request(MediaType.APPLICATION_JSON);
-		Response response = invocationBuilder.get();
-		if (response.getStatus() != Status.OK.getStatusCode()) {
-			logger.error("Error connecting with the server. Code: {}",response.getStatus());
-			return "Error";
-		} else {
-			String responseMessage = response.readEntity(String.class);
-			logger.info("* Customers coming from the server: '{}'", responseMessage);
-			return responseMessage;
-		}
-	}
-	
-	public boolean deleteVehicleBoolean(String numberPlate) {
-	    WebTarget deleteVehicleWebTarget = webTarget.path("deletevehicle");
+        new DCServerManager(hostname, port);
+    }
 
-	    Response response = deleteVehicleWebTarget
-	            .queryParam("numberPlate", numberPlate)
-	            .request(MediaType.APPLICATION_JSON)
-	            .delete();
+    /**
+     * Retrieves a string representation of the customers from the server.
+     * 
+     * @return A string representation of the customers.
+     */
+    public String getCustomersString() {
+        WebTarget DeustoCarsWebTarget = webTarget.path("server/getcustomers");
+        Invocation.Builder invocationBuilder = DeustoCarsWebTarget.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.get();
+        if (response.getStatus() != Status.OK.getStatusCode()) {
+            logger.error("Error connecting with the server. Code: {}", response.getStatus());
+            return "Error";
+        } else {
+            String responseMessage = response.readEntity(String.class);
+            logger.info("* Customers coming from the server: '{}'", responseMessage);
+            return responseMessage;
+        }
+    }
 
-	    if (response.getStatus() != Response.Status.OK.getStatusCode()) {
-	        logger.info("Error connecting with the server. Code: {}", response.getStatus());
-	        return false;
-	    } else {
-	        logger.info("Vehicle correctly deleted");
-	        return true;
-	    }
-	}
-	public String getVehiclesString(){
-		WebTarget DeustoCarsWebTarget = webTarget.path("server/getvehicles");
-		Invocation.Builder invocationBuilder = DeustoCarsWebTarget.request(MediaType.APPLICATION_JSON);
-		Response response = invocationBuilder.get();
-		if (response.getStatus() != Status.OK.getStatusCode()) {
-			logger.error("Error connecting with the server. Code: {}",response.getStatus());
-			return "Error";
-		} else {
-			String responseMessage = response.readEntity(String.class);
-			logger.info("* Vehicles coming from the server: '{}'", responseMessage);
-			return responseMessage;
-		}
-	}
+    /**
+     * Deletes a vehicle from the server.
+     * 
+     * @param numberPlate The number plate of the vehicle to delete.
+     * @return True if the vehicle was deleted successfully, false otherwise.
+     */
+    public boolean deleteVehicleBoolean(String numberPlate) {
+        WebTarget deleteVehicleWebTarget = webTarget.path("deletevehicle");
 
-	@Override
-	public void run() {
-		running.set(true);
-		while(running.get()) {
-			try {
-				Thread.sleep(2000);
-				System.out.println("Obtaining data from server...");
-				//DeustoCarsInfo DeustoCarsInfo = getDeustoCarsInfo();
-				this.customerText.setText(getCustomersString());
-				this.vehicleText.setText(getVehiclesString());
-			} catch (InterruptedException e){ 
+        Response response = deleteVehicleWebTarget.queryParam("numberPlate", numberPlate)
+                .request(MediaType.APPLICATION_JSON).delete();
+
+        if (response.getStatus() != Response.Status.OK.getStatusCode()) {
+            logger.info("Error connecting with the server. Code: {}", response.getStatus());
+            return false;
+        } else {
+            logger.info("Vehicle correctly deleted");
+            return true;
+        }
+    }
+
+    /**
+     * Retrieves a string representation of the vehicles from the server.
+     * 
+     * @return A string representation of the vehicles.
+     */
+    public String getVehiclesString() {
+        WebTarget DeustoCarsWebTarget = webTarget.path("server/getvehicles");
+        Invocation.Builder invocationBuilder = DeustoCarsWebTarget.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.get();
+        if (response.getStatus() != Status.OK.getStatusCode()) {
+            logger.error("Error connecting with the server. Code: {}", response.getStatus());
+            return "Error";
+        } else {
+            String responseMessage = response.readEntity(String.class);
+            logger.info("* Vehicles coming from the server: '{}'", responseMessage);
+            return responseMessage;
+        }
+    }
+
+    @Override
+    public void run() {
+        running.set(true);
+        while (running.get()) {
+            try {
+                Thread.sleep(2000);
+                System.out.println("Obtaining data from server...");
+                this.customerText.setText(getCustomersString());
+                this.vehicleText.setText(getVehiclesString());
+            } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 System.out.println("Thread was interrupted, Failed to complete operation");
             }
-		}
-	}
+        }
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		JButton target = (JButton)e.getSource();
-		if (target == this.buttonEnd) {
-			this.stop();
-			System.exit(0);
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton target = (JButton) e.getSource();
+        if (target == this.buttonEnd) {
+            this.stop();
+            System.exit(0);
+        }
+    }
 
-	public void stop() {
-		this.running.set(false);
-	}
+    /**
+     * Stops the DCServerManager.
+     */
+    public void stop() {
+        this.running.set(false);
+    }
 
 }
