61dbb0edaf95801c7423a74b021c74ecaf4cd47a
Pablo Herrero <pabloherrero@opendeusto.es>
2024-04-29 18:19:46 +0200

    Populate DB sql

---
+++ DeustoCars/sql/populate.sql
@@ -0,0 +1,15 @@
+USE deustoCarsDB;
+
+-- Insert customer data
+INSERT INTO customers (email, name, surname, birth_date) 
+VALUES 
+    ('test@gmail.com', 'Billy', 'Bob', '2000-01-01'),
+    ('test2@gmail.com', 'Johnny', 'Jones', '1995-05-15'),
+    ('test3@gmail.com', 'Evelyn', 'Easton', '1988-11-20');
+
+-- Insert vehicle data
+INSERT INTO vehicles (number_plate, brand, model, ready_to_borrow, on_repair)
+VALUES 
+    ('9872SLY', 'Toyota', 'Corolla', true, false),
+    ('1234QWR', 'Opel', 'Corsa', true, false),
+    ('5678BNM', 'Volkswagen', 'Golf', true, false);
