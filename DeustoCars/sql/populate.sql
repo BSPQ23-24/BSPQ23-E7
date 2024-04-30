c19997269486b7bcc777d3717059ce2b942495a9
Asitulom <asieriturriotz@opendeusto.es>
2024-04-30 18:28:15 +0200

    add to populate.sql

--- DeustoCars/sql/populate.sql
+++ DeustoCars/sql/populate.sql
@@ -6,6 +6,7 @@ VALUES
     ('test@gmail.com', 'Billy', 'Bob', '2000-01-01'),
     ('test2@gmail.com', 'Johnny', 'Jones', '1995-05-15'),
     ('test3@gmail.com', 'Evelyn', 'Easton', '1988-11-20');
+    ('x@gmail.com', 'xx', 'xxxx', '2002-10-31');
 
 -- Insert vehicle data
 INSERT INTO vehicles (number_plate, brand, model, ready_to_borrow, on_repair)
@@ -13,3 +14,4 @@ VALUES
     ('9872SLY', 'Toyota', 'Corolla', true, false),
     ('1234QWR', 'Opel', 'Corsa', true, false),
     ('5678BNM', 'Volkswagen', 'Golf', true, false);
+    ('123ABC', 'Toyota', 'Corolla', true, false);
