f77487a0d625b3c6aefbbba84bdd45e7e4b0e42b
Asitulom <asieriturriotz@opendeusto.es>
2024-05-15 11:36:30 +0200

    git hub pages

--- DeustoCars/docs/search/functions_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['actionperformed_0',['actionPerformed',['../classes_1_1deusto_1_1spq_1_1server_1_1_d_c_server_manager.html#acf35d905804c8dc896031deb16e518ed',1,'es::deusto::spq::server::DCServerManager']]],
-  ['addcustomer_1',['addCustomer',['../classes_1_1deusto_1_1spq_1_1server_1_1_d_c_server.html#ac9118cb014662fcf7f5e25adcb0ec12a',1,'es::deusto::spq::server::DCServer']]],
-  ['addmessage_2',['addMessage',['../classes_1_1deusto_1_1spq_1_1server_1_1jdo_1_1_user.html#a2b03c52150cafe2e57850dc3463bdb88',1,'es::deusto::spq::server::jdo::User']]],
-  ['addvehicle_3',['addVehicle',['../classes_1_1deusto_1_1spq_1_1server_1_1_d_c_server.html#a2ec1d7f0f08315b839756dae4ce2d7c9',1,'es::deusto::spq::server::DCServer']]]
-];
